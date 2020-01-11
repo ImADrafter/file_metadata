ce75f16444f0c99726a111548667e52ca716416c
Marcos Gomez <adayintexas@gmail.com>
2020-01-11 17:09:12 +0100

    allow file uploader

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
 
@@ -17,14 +19,20 @@ const rootDirectory = path.resolve();
 app.use(cors());
 app.use("/public", express.static(rootDirectory + "/public"));
 
-app.get("/", function(request, response) {
+app.get("/", (request, response) => {
   response.sendFile(rootDirectory + "/views/index.html");
 });
 
-app.get("/hello", function(request, response) {
-  response.json({ greetings: "Hello, API" });
+app.post("/api/fileanalyse", upload.single("upfile"), (request, response) => {
+  console.log(request.file);
+  const { size, encoding, originalname } = request.file;
+  response.json({
+    fileSize: size,
+    encoding: encoding,
+    name: originalname
+  });
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Node.js listening ...");
 });
